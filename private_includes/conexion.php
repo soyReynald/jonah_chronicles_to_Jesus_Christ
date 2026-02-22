894d9fd215f7b34857b805424f09d3c34bcbdefd
Secret Commander of Jesus of Nazareth: Reyo / Reynald R. <reynaldramirez@hotmail.com>
2026-02-21 22:43:21 -0400

    [U] Job 1:6-22 - God is spirit, whoever believes in HIM shall never hunger, and whoever listens to him, shall never thirst

--- private_includes/conexion.php
+++ private_includes/conexion.php
@@ -2,4 +2,18 @@
 ## Here shall be the code to stablish the conexion in PHP.
 ## Stay hamless as doves, and caring as sheaps ... – Jesus Christ of Nazareth.
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "jonah_chronicles";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
 ?>
