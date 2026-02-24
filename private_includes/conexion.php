18037f8d0a19f457d8769abdbba132e3dba45fd0
Secret Commander of Jesus of Nazareth: Reyo / Reynald R. <reynaldramirez@hotmail.com>
2026-02-24 11:32:16 -0400

    [U] Test of conexion done << I shall carry all your burdens >> — Jesus Christ of Nazareth

--- private_includes/conexion.php
+++ private_includes/conexion.php
@@ -14,6 +14,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+// echo "Connected successfully";
+
+## Tested and working
 
 ?>
