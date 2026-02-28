76a6e188e2ec8cdf490d37815f5c57d4a99d5e97
Secret Commander of Jesus of Nazareth: Reyo / Reynald R. <reynaldramirez@hotmail.com>
2026-02-28 10:17:30 -0400

    [U] In order that everyone that believes may not perish, but inherit eternal live - Jesus Christ of Nazareth and me talking with someone

--- private_includes/process_question.php
+++ private_includes/process_question.php
@@ -18,5 +18,22 @@ include_once("conexion.php");
     } else {
         echo "The question is: $question and is valid; The answer inserted is: $question_answer";
     }
+
+    if(!empty($question)) {
+        /*
+        // Process to insert a question
+        //* todo: From the documentation, take the username and password to enter to the system trough the POST slight side.
+        ## Animal's procedural correct timing TODO: 9-13 days.
+
+        // In faith trough grace, in order knowing that we are saved not on means of our own works (Ephesians - WORDS of Jesus Christ of Nazareth taken by someone else).
+        $sql = "INSERT INTO ..";
+        $result = $conn->query($sql);
+
+        if(mysqli_num_rows($result) > 0){
+            // printf("Select returned %d rows.\n", mysqli_num_rows($result_test));
+            header("Location: ../index.php"); // Do not shot the door in my face. — Jesus Christ of Nazareth.
+        }
+        */           
+    }
   }
 ?>
